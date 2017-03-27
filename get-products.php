4c25f21474840f1cc533095f9d7a59f8e9692dc1
Webfactory Online Stores <webfactoryos@gmail.com>
2017-03-27 17:02:00 +0000

    uncommented code

--- get-products.php
+++ get-products.php
@@ -13,9 +13,9 @@ if (!isset($query['shop'])) {
 }
 
 $store = $query['shop']; 
-echo 'echoing store';
+echo 'echoing store ';
 echo $store;
-/* $select = $db->query("SELECT access_token FROM installs WHERE store = '$store'");
+$select = $db->query("SELECT access_token FROM installs WHERE store = '$store'");
  $user = $select->fetch_object();
 $access_token = $user->access_token;
 echo 'echoing access token';
@@ -40,8 +40,8 @@ $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, [
     'cache' => 'cache',
     'debug' => true
-]); */
+]); 
 
 echo 'down to template';
-// $template = $twig->loadTemplate('products.html');
-// echo $template->render(['products' => $result['products']]);
+$template = $twig->loadTemplate('products.html');
+echo $template->render(['products' => $result['products']]);
